a852799e199a08f4a5c9271d9eb9352e5e4d6935
Александр Конев <aleksandrkonev@MacBook-Pro-Aleksandr.local>
2024-08-18 19:41:04 +0300

    Implement localStorage persistence for Redux store: added functions to save and load state

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,13 +1,36 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 import rootReducer from '../reducers';
+import { saveToLocalStorage } from '../../utils/localStorage';
 
 // Redux DevTools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+// Load state from localStorage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('appState');
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.error('Could not load state', e);
+    return undefined;
+  }
+};
+
+const preloadedState = loadState();
+
 const store = createStore(
   rootReducer,
+  preloadedState, // Initialize store with the preloaded state
   composeEnhancers(applyMiddleware(thunk)),
 );
+
+// Save state to localStorage on each state change
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
+});
 
 export default store;
